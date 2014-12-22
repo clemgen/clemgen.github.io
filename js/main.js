e7ef276f8fd77b124be63b04c753f3d370576419
dimitrie <Bender@Benders-MacBook-Pro.local>
2014-12-22 16:00:27 +0200

    Now it's something!

---
+++ js/main.js
@@ -0,0 +1,15 @@
+/*global bootstrap:true, Modernizr:true */
+
+$( document ).ready(function() {
+
+	$(".animateMe").children().velocity("transition.flipXIn", {duration: 600, stagger:200});
+	$(".animateMe").click(function(){
+		$(this).velocity("callout.pulse", 300);
+	});
+
+	$("i").hover(function(){
+		$(this).velocity({scale: 0.9}, 100);
+	}, function(){
+		$(this).velocity("stop").velocity({scale: 1}, 100);
+	});
+});
---
+++ js/min/main-min.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){$(".animateMe").children().velocity("transition.flipXIn",{duration:600,stagger:200}),$(".animateMe").click(function(){$(this).velocity("callout.pulse",300)}),$("i").hover(function(){$(this).velocity({scale:.9},100)},function(){$(this).velocity("stop").velocity({scale:1},100)})});
